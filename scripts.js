a47de588d31969840c951cc0b26ac0d273d21720
Sean <fiveironcaddy488@sbcglobal.net>
2020-02-20 17:45:56 -0800

    changed filepaths to subpages

--- scripts.js
+++ scripts.js
@@ -2,20 +2,20 @@ var page = new Array();
 var pageIndex = 0;
 
 //characters
-page[0] = "http://sean.jcduffyphoto.com/FinalProj/partymain/partyhome.html"
-page[1] = "http://sean.jcduffyphoto.com/FinalProj/partymain/enoch.html"
-page[2] = "http://sean.jcduffyphoto.com/FinalProj/partymain/scanlan.html"
-page[3] = "http://sean.jcduffyphoto.com/FinalProj/partymain/isenhart.html"
-page[4] = "http://sean.jcduffyphoto.com/FinalProj/partymain/dominus.html"
-page[5] = "http://sean.jcduffyphoto.com/FinalProj/partymain/estelle.html"
-page[6] = "http://sean.jcduffyphoto.com/FinalProj/partymain/delle.html"
-page[7] = "http://sean.jcduffyphoto.com/FinalProj/partymain/olgregg.html"
+page[0] = "partymain/partyhome.html"
+page[1] = "partymain/enoch.html"
+page[2] = "partymain/scanlan.html"
+page[3] = "partymain/isenhart.html"
+page[4] = "partymain/dominus.html"
+page[5] = "partymain/estelle.html"
+page[6] = "partymain/delle.html"
+page[7] = "partymain/olgregg.html"
 
 //world
-page[8] = "http://sean.jcduffyphoto.com/FinalProj/worldmain/worldhome.html"
-page[9] = "http://sean.jcduffyphoto.com/FinalProj/worldmain/history.html"
-page[10] = "http://sean.jcduffyphoto.com/FinalProj/worldmain/geography.html"
-page[11] = "http://sean.jcduffyphoto.com/FinalProj/worldmain/demographics.html"
+page[8] = "worldmain/worldhome.html"
+page[9] = "worldmain/history.html"
+page[10] = "worldmain/geography.html"
+page[11] = "worldmain/demographics.html"
 
 function subpage(n) {
 	pageIndex = n;
